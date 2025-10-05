d4b90093c2c16613b703b19ac37e65eaa74a40c7
connosieur <hechemm_us@protonmail.com>
2025-10-05 00:20:12 -0500

    Update hello_server_test.go

--- hello_server_test.go
+++ hello_server_test.go
@@ -19,7 +19,7 @@ func TestGreetingSpecificDemo(t *testing.T) {
 func TestShowFailure(t *testing.T) {
 	greeting := CreateGreeting("Demo1")
 	if greeting != "Hello, Demo\n" {
-		t.Errorf("Intentional failure. got: %s, want: %s.", greeting, "Hello, Demo\n")
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
 	}
 }
 
